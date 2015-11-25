b91e090642d85e3afffcfce75db8141f2641353c
kainlite <kainlite@gmail.com>
2015-11-25 13:39:33 -0300

    Not sure about mongo yet for this project

--- src/misc/database.c
+++ src/misc/database.c
@@ -3,21 +3,20 @@
 #include <mongoc.h>
 #include <misc/database.h>
 
-void	mongo_connect(char *db_name)
+void	mongo_connect(char *database_name, char *collection_name)
 {
 	mongoc_client_t      *client;
 	bson_t               *insert;
 	bson_error_t          error;
 	mongoc_collection_t  *collection;
 
-	printf("Connecting to database: %s...\n", db_name);
+	printf("Connecting to database: %s... on collection: %s\n", database_name, collection_name);
 
 	client = mongoc_client_new("mongodb://localhost:27017");
 
-	mongoc_client_get_database(client, db_name);
+	mongoc_client_get_database(client, database_name);
 
-
-	collection = mongoc_client_get_collection(client, db_name, "coll_name");
+	collection = mongoc_client_get_collection(client, database_name, collection_name);
 
 	insert = BCON_NEW ("hello", BCON_UTF8 ("world"));
 
--- src/misc/database.h
+++ src/misc/database.h
@@ -1,1 +1,1 @@
-void mongo_connect(char *db_name);
+void mongo_connect(char *database_name, char *collection_name);
